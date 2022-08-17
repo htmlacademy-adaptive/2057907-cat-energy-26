f0302abf1835badf60a10547aac7260f8c1e3af1
Keks <keks@htmlacademy.ru>
2022-08-17 19:07:23 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/header.js
@@ -0,0 +1,22 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+let navList = document.querySelector('.main-nav__list');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+  if (navList.classList.contains('main-nav__list--closed')) {
+      navList.classList.remove('main-nav__list--closed');
+      navList.classList.add('main-nav__list--opened');
+    } else {
+      navList.classList.add('main-nav__list--closed');
+      navList.classList.remove('main-nav__list--opened');
+    }
+});
